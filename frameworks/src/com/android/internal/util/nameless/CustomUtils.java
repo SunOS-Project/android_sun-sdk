29e9968b96bc68ade03da0b7c2dc7eb653a92d4a
AshutoshSundresh <ashutoshsundresh@gmail.com>
2024-01-06 14:04:03 +0800

    SystemUI: Integrate Google Lens into Screenshot UI
    Co-authored-by: cjh1249131356 <cjh1249131356@gmail.com>
    Signed-off-by: cjh1249131356 <cjh1249131356@gmail.com>

---
+++ frameworks/src/com/android/internal/util/nameless/CustomUtils.java
@@ -0,0 +1,31 @@
+/*
+ * Copyright (C) 2022-2024 The Nameless-AOSP Project
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+package com.android.internal.util.nameless;
+
+import android.content.Context;
+import android.content.pm.PackageInfo;
+import android.content.pm.PackageManager.NameNotFoundException;
+
+public class CustomUtils {
+
+    public static boolean isPackageInstalled(Context context, String pkg) {
+        return isPackageInstalled(context, pkg, true);
+    }
+
+    public static boolean isPackageInstalled(Context context, String pkg, boolean ignoreState) {
+        if (pkg != null) {
+            try {
+                final PackageInfo pi = context.getPackageManager().getPackageInfo(pkg, 0);
+                if (!pi.applicationInfo.enabled && !ignoreState) {
+                    return false;
+                }
+            } catch (NameNotFoundException e) {
+                return false;
+            }
+        }
+        return true;
+    }
+}
