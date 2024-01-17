7dc59d0f1cc09c39b6f6f2b4893f5ae00656b2f1
cjh1249131356 <cjh1249131356@gmail.com>
2024-01-18 00:19:52 +0800

    Settings: Check system feature for vibration toggle visibility
    Signed-off-by: cjh1249131356 <cjh1249131356@gmail.com>

--- Settings/src/org/nameless/settings/accessibility/FaceUnlockVibrationPreferenceController.java
+++ Settings/src/org/nameless/settings/accessibility/FaceUnlockVibrationPreferenceController.java
@@ -6,14 +6,24 @@
 package org.nameless.settings.accessibility;
 
 import android.content.Context;
+import android.content.pm.PackageManager;
 import android.provider.Settings;
 
 import org.nameless.provider.SettingsExt;
 
 public class FaceUnlockVibrationPreferenceController extends CustomVibrationTogglePreferenceController {
 
+    private final PackageManager mPackageManager;
+
     public FaceUnlockVibrationPreferenceController(Context context, String preferenceKey) {
         super(context, preferenceKey, new CustomVibrationPreferenceConfig(
                 context, Settings.System.VIBRATE_ON, SettingsExt.System.CUSTOM_HAPTIC_ON_FACE));
+        mPackageManager = context.getPackageManager();
+    }
+
+    @Override
+    public int getAvailabilityStatus() {
+        return mPackageManager.hasSystemFeature(PackageManager.FEATURE_FACE)
+                ? AVAILABLE : UNSUPPORTED_ON_DEVICE;
     }
 }
