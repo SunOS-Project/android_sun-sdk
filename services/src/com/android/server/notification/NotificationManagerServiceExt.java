51871496a821a4dfc65bfb3b02bcd6eb093c0c2d
cjh1249131356 <cjh1249131356@gmail.com>
2024-02-03 22:47:56 +0800

    services: Create NotificationManagerServiceExt to reduce frameworks change
    Signed-off-by: cjh1249131356 <cjh1249131356@gmail.com>

---
+++ services/src/com/android/server/notification/NotificationManagerServiceExt.java
@@ -0,0 +1,76 @@
+/*
+ * Copyright (C) 2024 The Nameless-AOSP Project
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+package com.android.server.notification;
+
+import static org.nameless.provider.SettingsExt.System.VIBRATION_PATTERN_NOTIFICATION;
+
+import android.content.ContentResolver;
+import android.content.Context;
+import android.database.ContentObserver;
+import android.net.Uri;
+import android.os.UserHandle;
+import android.os.VibrationEffect;
+import android.provider.Settings;
+
+import org.nameless.os.VibrationPatternManager;
+import org.nameless.os.VibrationPatternManager.Type;
+
+class NotificationManagerServiceExt {
+
+    private static class InstanceHolder {
+        private static final NotificationManagerServiceExt INSTANCE = new NotificationManagerServiceExt();
+    }
+
+    static NotificationManagerServiceExt getInstance() {
+        return InstanceHolder.INSTANCE;
+    }
+
+    private static final Uri URI_VIBRATION_PATTERN_NOTIFICATION =
+            Settings.System.getUriFor(VIBRATION_PATTERN_NOTIFICATION);
+
+    private ContentResolver mResolver;
+
+    private VibrationEffect mCustomVibrationEffect;
+
+    private boolean mNeedUpdateVibration = false;
+
+    void init(Context context) {
+        mResolver = context.getContentResolver();
+    }
+
+    void observe(ContentObserver observer) {
+        mResolver.registerContentObserver(URI_VIBRATION_PATTERN_NOTIFICATION,
+                false, observer, UserHandle.USER_ALL);
+    }
+
+    void updateSettings(Uri uri) {
+        if (uri == null || URI_VIBRATION_PATTERN_NOTIFICATION.equals(uri)) {
+            updateCustomVibrationEffect();
+        }
+    }
+
+    void setNeedUpdateVibration() {
+        mNeedUpdateVibration = true;
+    }
+
+    boolean checkNeedUpdateVibrationAndReset() {
+        final boolean ret = mNeedUpdateVibration;
+        mNeedUpdateVibration = false;
+        return ret;
+    }
+
+    VibrationEffect getVibrationEffect() {
+        return mCustomVibrationEffect;
+    }
+
+    private void updateCustomVibrationEffect() {
+        final int patternNumber = Settings.System.getIntForUser(
+                mResolver, VIBRATION_PATTERN_NOTIFICATION,
+                0, UserHandle.USER_CURRENT);
+        mCustomVibrationEffect = VibrationPatternManager.getVibrationFromNumber(
+                patternNumber, Type.NOTIFICATION);
+    }
+}
