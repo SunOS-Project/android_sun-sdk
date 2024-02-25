e048316add9df95b322227d5716bf3291332bfd5
cjh1249131356 <cjh1249131356@gmail.com>
2024-02-26 01:30:30 +0800

    frameworks: ApplicationPackageManagerExt: Cleanup
    Signed-off-by: cjh1249131356 <cjh1249131356@gmail.com>

--- frameworks/src/android/app/ApplicationPackageManagerExt.java
+++ frameworks/src/android/app/ApplicationPackageManagerExt.java
@@ -16,10 +16,7 @@ class ApplicationPackageManagerExt {
 
     private static final Set<String> FEATURES_NEXUS = Set.of(
         "com.google.android.apps.photos.NEXUS_PRELOAD",
-        "com.google.android.apps.photos.nexus_preload",
-        "com.google.android.feature.PIXEL_EXPERIENCE",
-        "com.google.android.feature.GOOGLE_BUILD",
-        "com.google.android.feature.GOOGLE_EXPERIENCE"
+        "com.google.android.apps.photos.nexus_preload"
     );
 
     private static final Set<String> FEATURES_PIXEL = Set.of(
@@ -37,24 +34,6 @@ class ApplicationPackageManagerExt {
         "com.google.android.feature.PIXEL_EXPERIENCE",
         "com.google.android.feature.GOOGLE_BUILD",
         "com.google.android.feature.GOOGLE_EXPERIENCE"
-    );
-
-    private static final Set<String> FEATURES_PIXEL_OTHERS = Set.of(
-        "com.google.android.feature.ASI",
-        "com.google.android.feature.ANDROID_ONE_EXPERIENCE",
-        "com.google.android.feature.GOOGLE_FI_BUNDLED",
-        "com.google.android.feature.LILY_EXPERIENCE",
-        "com.google.android.feature.TURBO_PRELOAD",
-        "com.google.android.feature.WELLBEING",
-        "com.google.lens.feature.IMAGE_INTEGRATION",
-        "com.google.lens.feature.CAMERA_INTEGRATION",
-        "com.google.photos.trust_debug_certs",
-        "com.google.android.feature.AER_OPTIMIZED",
-        "com.google.android.feature.NEXT_GENERATION_ASSISTANT",
-        "android.software.game_service",
-        "com.google.android.feature.EXCHANGE_6_2",
-        "com.google.android.apps.dialer.call_recording_audio",
-        "com.google.android.apps.dialer.SUPPORTED"
     );
 
     private static final Set<String> FEATURES_TENSOR = Set.of(
@@ -76,16 +55,9 @@ class ApplicationPackageManagerExt {
         final String packageName = ActivityThread.currentPackageName();
         if ("com.google.android.apps.photos".equals(packageName)) {
             if (FEATURES_PIXEL.contains(name)) return HAS_FEATURE_NO;
-            if (FEATURES_PIXEL_OTHERS.contains(name)) return HAS_FEATURE_YES;
-            if (FEATURES_NEXUS.contains(name)) return HAS_FEATURE_YES;
-        }
-        if ("com.google.android.googlequicksearchbox".equals(packageName)) {
-            if (FEATURES_PIXEL.contains(name)) return HAS_FEATURE_YES;
-            if (FEATURES_PIXEL_OTHERS.contains(name)) return HAS_FEATURE_YES;
             if (FEATURES_NEXUS.contains(name)) return HAS_FEATURE_YES;
         }
         if (FEATURES_PIXEL.contains(name)) return HAS_FEATURE_YES;
-        if (FEATURES_PIXEL_OTHERS.contains(name)) return HAS_FEATURE_YES;
         return HAS_FEATURE_UNHANDLED;
     }
 }
