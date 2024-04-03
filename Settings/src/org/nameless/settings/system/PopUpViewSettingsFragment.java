2dbde22055af7d1819e20e963fdeec28058089d6
cjh1249131356 <cjh1249131356@gmail.com>
2024-04-03 21:57:02 +0800

    Settings: Launch Pop-Up View manage apps activity with no history flag
    Signed-off-by: cjh1249131356 <cjh1249131356@gmail.com>

--- Settings/src/org/nameless/settings/system/PopUpViewSettingsFragment.java
+++ Settings/src/org/nameless/settings/system/PopUpViewSettingsFragment.java
@@ -6,6 +6,7 @@
 package org.nameless.settings.system;
 
 import android.app.settings.SettingsEnums;
+import android.content.Intent;
 import android.os.Bundle;
 
 import androidx.preference.Preference;
@@ -22,6 +23,7 @@ public class PopUpViewSettingsFragment extends SettingsPreferenceFragment implem
         Preference.OnPreferenceChangeListener {
 
     private static final String KEY_GESTURE = "system_tool_windowing_mode_gesture";
+    private static final String KEY_MANAGE_APP = "pop_up_view_manage_apps";
     private static final String KEY_MORE_CIRCLES = "system_tool_more_circles";
     private static final String KEY_KEEP_MUTE = "pop_up_keep_mute_in_mini";
     private static final String KEY_SINGLE_TAP_ACTION = "pop_up_single_tap_action";
@@ -31,6 +33,7 @@ public class PopUpViewSettingsFragment extends SettingsPreferenceFragment implem
     private static final String KEY_NOTIFICATION_BLACKLIST = "pop_up_notification_blacklist";
 
     private SystemSettingSwitchPreference mGlobalGesture;
+    private Preference mManageApps;
     private SystemSettingSwitchPreference mShowMoreCircles;
     private SystemSettingSwitchPreference mKeepMuteInMini;
     private SystemSettingListPreference mSingleTapAction;
@@ -45,6 +48,7 @@ public class PopUpViewSettingsFragment extends SettingsPreferenceFragment implem
         addPreferencesFromResource(R.xml.pop_up_view_settings);
 
         mGlobalGesture = (SystemSettingSwitchPreference) findPreference(KEY_GESTURE);
+        mManageApps = (Preference) findPreference(KEY_MANAGE_APP);
         mShowMoreCircles = (SystemSettingSwitchPreference) findPreference(KEY_MORE_CIRCLES);
         mKeepMuteInMini = (SystemSettingSwitchPreference) findPreference(KEY_KEEP_MUTE);
         mSingleTapAction = (SystemSettingListPreference) findPreference(KEY_SINGLE_TAP_ACTION);
@@ -54,6 +58,21 @@ public class PopUpViewSettingsFragment extends SettingsPreferenceFragment implem
         mNotifBlacklist = (Preference) findPreference(KEY_NOTIFICATION_BLACKLIST);
 
         mGlobalGesture.setChecked(PopUpSettingsHelper.isGestureEnabled(getContext()));
+
+        mManageApps.setOnPreferenceClickListener(p -> {
+            final Intent intent = new Intent();
+            intent.setClassName("org.nameless.systemtool",
+                    "org.nameless.systemtool.windowmode.ManageAppsActivity");
+            intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK |
+                    Intent.FLAG_ACTIVITY_NO_HISTORY |
+                    Intent.FLAG_ACTIVITY_EXCLUDE_FROM_RECENTS);
+
+            try {
+                getContext().startActivity(intent);
+            } catch (Exception ignored) {}
+
+            return true;
+        });
 
         mShowMoreCircles.setChecked(PopUpSettingsHelper.shouldShowMoreCircles(getContext()));
 
