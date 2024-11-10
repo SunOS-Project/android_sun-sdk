ef3a72bb793c6e27ebb84b01a66c6753124c3806
cjh1249131356 <cjh1249131356@gmail.com>
2024-11-10 18:24:15 +0800

    sdk: Allow to use back gesture on keyboard
    Signed-off-by: cjh1249131356 <cjh1249131356@gmail.com>

---
+++ frameworks/src/android/inputmethodservice/InputMethodServiceExt.java
@@ -0,0 +1,23 @@
+/*
+ * Copyright (C) 2024 The Nameless-AOSP Project
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+package android.inputmethodservice;
+
+import android.os.SystemProperties;
+
+/** @hide */
+public class InputMethodServiceExt {
+
+    private static final String PROP_ALLOW_BACK_GESTURE_ON_IME =
+            "persist.sys.nameless.keyboard.back_gesture";
+
+    public static boolean getAllowBackGestureOnIme() {
+        return SystemProperties.getBoolean(PROP_ALLOW_BACK_GESTURE_ON_IME, true);
+    }
+
+    public static void setAllowBackGestureOnIme(boolean allow) {
+        SystemProperties.set(PROP_ALLOW_BACK_GESTURE_ON_IME, allow ? "true" : "false");
+    }
+}
